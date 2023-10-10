d8ab0ed8fabb69331afa29503c7e2f2ed7be7ea1
Jun3-Alex <130096011+Jun3-Alex@users.noreply.github.com>
2023-10-11 01:54:26 +0500

    add fields

---
+++ Notebook.java
@@ -0,0 +1,51 @@
+package Lesson6.Hometask;
+
+public class Notebook {
+    private String model;
+    private int ram;
+    private int hardDriveCapacity;
+    private String operatingSystem;
+
+    private String colour;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getHardDriveCapacity() {
+        return hardDriveCapacity;
+    }
+
+    public void setHardDriveCapacity(int hardDriveCapacity) {
+        this.hardDriveCapacity = hardDriveCapacity;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    public String getColour() {
+
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+}
