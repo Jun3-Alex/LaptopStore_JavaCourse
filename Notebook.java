ecff159ff932600f69201d8e264dd72323835313
Jun3-Alex <130096011+Jun3-Alex@users.noreply.github.com>
2023-10-11 15:38:21 +0500

    add random fill models

--- Notebook.java
+++ Notebook.java
@@ -1,19 +1,34 @@
 package Lesson6.Hometask;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Notebook {
     private String model;
     private int ram;
     private int hardDriveCapacity;
     private String operatingSystem;
-
     private String colour;
+    private List<String> availableModels;
+    public Notebook(){
+        availableModels = new ArrayList<>();
+        availableModels.add("Asus");
+        availableModels.add("Lenovo");
+        availableModels.add("Macbook");
+        availableModels.add("Vivobook");
+        availableModels.add("MSI");
+    }
 
     public String getModel() {
         return model;
     }
 
-    public void setModel(String model) {
-        this.model = model;
+    public void setModel() {
+
+        Random random = new Random();
+        int randomIndex = random.nextInt(availableModels.size());
+        model = availableModels.get(randomIndex);
     }
 
     public int getRam() {
@@ -41,11 +56,17 @@ public class Notebook {
     }
 
     public String getColour() {
-
         return colour;
     }
 
     public void setColour(String colour) {
         this.colour = colour;
     }
+
+    @Override
+    public String toString() {
+        return String.format("Model: %s", model);
+    }
 }
+
+
